a21d38de6b58c0d70709bfe5b348c1afbd5d98e8
Chris Hundley <bottompair@gmail.com>
2015-05-25 17:07:59 -0700

    added bk and aq status to meta data

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -172,6 +172,8 @@ exports.getMeta = function(req, res, next) {
                 current_clan: user.current_clan,
                 role: user.role.title,
                 avatar: user.profile.avatar,
+                hasBK: true ? user.profile.heroes.bk > 0 : false,
+                hasAQ: true ? user.profile.heroes.aq > 0 : false,
                 bkUpgrade: user.profile.bkUpgrade,
                 aqUpgrade: user.profile.aqUpgrade
             };
