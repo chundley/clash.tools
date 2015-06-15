f9060f73d0640b4bfd903903851e65a763b14508
Chris Hundley <bottompair@gmail.com>
2015-06-15 14:37:23 -0700

    added some more help content

--- app/controllers/help-controller.js
+++ app/controllers/help-controller.js
@@ -10,13 +10,28 @@ function ($rootScope, $scope, $routeParams, $location, authService) {
     // initialize
     $rootScope.title = 'Help - clash.tools';
 
-    $scope.helpId = $routeParams.id;
+    //$scope.helpId = $routeParams.id;
 
-    $scope.helpContent = {
-        'av': {
+    $scope.helpIndex = 0;
+
+    $scope.helpContent = [
+        {
+            path: 'faq',
+            title: 'FAQ',
+            order: 100,
+            template: '/views/help/faq.html'
+        }, 
+        {       
             path: 'av',
             title: 'Attack Value',
+            order: 200,
             template: '/views/help/av.html'
+        }
+    ];
+
+    for (var idx=0; idx< $scope.helpContent.length; idx++) {
+        if ($scope.helpContent[idx].path == $routeParams.id) {
+            $scope.helpIndex = idx;
         }
     }
 
