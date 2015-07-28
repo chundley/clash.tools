db35c4e410453f58b7bc2543bbd24d78f7a7da47
Chris Hundley <bottompair@gmail.com>
2015-07-28 12:37:20 -0700

    prod troubleshooting

---
+++ app/controllers/test-controller.js
@@ -0,0 +1,4 @@
+angular.module('Clashtools.controllers')
+.controller('TestCtrl', ['$rootScope', '$scope', '$modal', '$window', 'moment', 'authService', 'sessionService', 'errorService', 'messagelogService', 'userService', 'playerNotesService', 'banListService', 'utils',
+function ($rootScope, $scope, $modal, $window, moment, authService, sessionService, errorService, messagelogService, userService, playerNotesService, banListService, utils) {
+}]);
