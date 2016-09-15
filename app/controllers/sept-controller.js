993e4219f9c323a98c6ffaed595a1977584cdf64
Chris Hundley <bottompair@gmail.com>
2016-09-15 12:10:43 -0700

    sept begging update

---
+++ app/controllers/sept-controller.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/*
+*   Controller for the Sept
+*/
+
+angular.module('Clashtools.controllers')
+.controller('SeptCtrl', ['$rootScope', '$scope', '$location', '$modal', 'md5', 'authService', 'mailService', 'errorService',
+function ($rootScope, $scope, $location, $modal, md5, authService, mailService, errorService) {
+
+    $rootScope.title = 'clash.tools - September update';
+
+
+}]);
--- app/public/js/webapp.js
+++ app/public/js/webapp.js
@@ -19,6 +19,7 @@ angular.module('Clashtools', ['ngRoute', 'ngCookies', 'ngAnimate', 'ngSanitize',
         .when('/logout', { controller: 'LogoutCtrl', templateUrl: '/views/login.html', access: access.public } )
         .when('/verify/:id', { controller: 'EmailVerifyCtrl', templateUrl: '/views/emailVerify.html', access: access.public } )
         .when('/pwreset/:id', { controller: 'PWResetCtrl', templateUrl: '/views/pwreset.html', access: access.public } )
+        .when('/sept', { controller: 'SeptCtrl', templateUrl: '/views/sept.html', access: access.public } )
         .when('/home', { controller: 'HomeCtrl', templateUrl: '/views/home.html', access: access.member } )
         .when('/profile', { controller: 'ProfileCtrl', templateUrl: '/views/profile.html', access: access.member } )
         .when('/results', { controller: 'ResultsCtrl', templateUrl: '/views/results.html', access: access.member } )
